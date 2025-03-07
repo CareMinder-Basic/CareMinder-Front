e18c8446e2466958654c05b9bf2e406472d5d78f
안승찬 <anseungchan@anseungchan-ui-MacBookPro-2.local>
2025-03-07 10:08:40 +0900

    .,

--- apps/CareFlow/src/app/layout.tsx
+++ apps/CareFlow/src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
   modal: React.ReactNode;
 }) {
-  const testScript ="test"
+  
   return (
     <html lang="ko">
       <body className={pretendard.className}>
