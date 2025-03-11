6edb08c2c4cdac57b1b04ec3be7148afc34a9a96
안승찬 <anseungchan@anseungchan-ui-MacBookPro-2.local>
2025-03-11 09:47:33 +0900

    .

--- packages/ui-library/.storybook/main.ts
+++ packages/ui-library/.storybook/main.ts
@@ -14,6 +14,7 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
+  staticDirs: ['../public', '../../fonts/dist'],
   async viteFinal(config) {
     return {
       ...config,
